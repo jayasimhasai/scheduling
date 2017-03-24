056b33d62dea48ad2c6dbd50d3d20ac308dbc276
Jayasimha Sai Koduri <jayasimhasai.koduri@engr-e132-d10.engr.siu.edu>
2017-03-24 14:34:46 -0500

    MFQ UPDATE

--- mfq_schedular.c
+++ mfq_schedular.c
@@ -3,89 +3,10 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <signal.h>
-
+#include "mfq.h"
+#define max	5
 int handler=1;
-struct node
-{
-	int pid,level;
-	char *name;
-
-}node;
-
-struct queue
-{
-struct node Node;
-struct queue* Next;
-
-}queue;
-
-struct queue* Head1=NULL;
-struct queue* Tail1=NULL;
-struct queue* Head2=NULL;
-struct queue* Tail2=NULL;
-
-void Enqueue(struct node ret)
-{
-	struct queue* temp=malloc(sizeof(queue));
-
-	temp->Node.pid = ret.pid;
-	temp->Node.name = ret.name;
-	temp->Node.level = ret.level;
-	temp->Next = NULL;
-	if(ret.level==1)
-	{
-		if(Head1 == NULL && Tail1 == NULL)
-		{
-			Head1=Tail1=temp;
-			return;
-		}
-		else
-		{
-		Tail1->Next= temp;
-		Tail1 = temp;
-		}
-	}
-	else
-	{
-		if(Head2 == NULL && Tail2 == NULL)
-		{
-			Head2=Tail2=temp;
-			return;
-		}
-		else
-		{
-		Tail2->Next= temp;
-		Tail2 = temp;
-		}
-	}
-
-
-}
-
-struct node Dequeue(int level)
-{	int pid;
-	struct queue* temp;
-	struct node ret;
-	if(level==1)
-	{
-	ret.pid=Head1->Node.pid;
-	ret.name=Head1->Node.name;
-	temp=Head1;
-	Head1=Head1->Next;
-	free(temp);
-	}
-	else
-	{
-	ret.pid=Head2->Node.pid;
-	ret.name=Head2->Node.name;
-	temp=Head2;
-	Head2=Head2->Next;
-	free(temp);	
-	}
-	return ret;
-
-}
-
+struct queue q[max];
 
 void term_child()
 {
@@ -98,22 +19,22 @@ void term_child()
 
 int main(int argc, char const *argv[])
 {
-struct node arg;
  signal(SIGCHLD, term_child);
-int pid,i,qt;
+int pid,i,qt,qc;
 
 
 //part:1   ************************************checking arguments********************************
 
-	if(argc>2)
+	if(argc>3)
 	{
 		qt = 1000*atoi(&argv[1][0]);
+		qc = atoi(&argv[2][0]);
 	}
 
 	else
 	{
 
-		printf("%s qt prog[1] prog[2] .... prog[n]",argv[0]);
+		printf("%s qt qc prog[1] prog[2] .... prog[n]",argv[0]);
 		exit(-1);
 
 	}
@@ -121,17 +42,14 @@ int pid,i,qt;
 
 //part:2   *************************************child creation************************************
 
-	for(i=2; i<argc; i++)
+	for(i=3; i<argc; i++)
 	    {
 	      
 
 	      printf("message from father: creating program %s\n", argv[i]);
 	     
 	      pid= fork();
-		arg.pid=pid;
-		arg.name=(char*) argv[i];
-		arg.level=1;
-	      Enqueue(arg);
+		
 	     
 	      if(pid==0)
 		{ 
@@ -140,6 +58,7 @@ int pid,i,qt;
 		   exit(0);
 		   
 		}
+		enqueue(pid,argv[i],&q[0]);
 
 	    }
 
@@ -149,22 +68,21 @@ int pid,i,qt;
 
 //part:3   ************************************scheduling********************************************
 	printf("I am the schedular i will begin scheduling my programs\n");
-   while(Head1!=NULL||Head2!=NULL)
-{	struct node ret;
-	while(Head1!=NULL)// scheduling loop
-	{	
-	      
+ for(int i=0;i<qc;i++)
+   while(q[i].head!=NULL)
+   {		      
 	      /* send the signal SIGCONT to the first element in queue */
-		kill(Head1->Node.pid,SIGCONT);
-		usleep(qt);
+		kill(q[i].head->pid,SIGCONT);
+		usleep((2*i+1)*qt);
 
 	 	if(handler)
 		{
-		    kill(Head1->Node.pid,SIGUSR1); 
+		    kill(q[i].head->pid,SIGUSR1); 
 		    usleep(1000);
-		    ret=Dequeue(1);
-		    ret.level=2;
-		    Enqueue(ret);   
+		    if(i==qc-1)
+	            enqueue(dequeue(&q[i]),NULL,&q[i]);  
+		    else
+		    enqueue(dequeue(&q[i]),NULL,&q[i+1]);   
 		   
 		  /* dequeue and re-enqueue */
 		    }
@@ -172,43 +90,14 @@ int pid,i,qt;
 		else
 		  {struct node ret;
 		    /* remove from list */
-		    ret=Dequeue(1);
-		    handler=1;
-		    printf("process %s is dead",ret.name);
+				    
+		    printf("process %d is dead",dequeue(&q[i]));
+			handler=1;
 		    /* mark chils as dead */
 		    }
 	       
 	    }
-	while(Head2!=NULL&&Head1==NULL)// scheduling loop
-	{	
-	      
-	      /* send the signal SIGCONT to the first element in queue */
-		kill(Head2->Node.pid,SIGCONT);
-		usleep(qt*2);
-
-	 	if(handler)
-		{
-		    kill(Head2->Node.pid,SIGUSR1); 
-		    usleep(1000);
-		    ret=Dequeue(2);
-		    ret.level=2;
-		    Enqueue(ret);   
-		   
-		  /* dequeue and re-enqueue */
-		    }
-
-		else
-		  {
-		    /* remove from list */
-		    ret=Dequeue(2);
-		    handler=1;
-		    printf("process %s is dead",ret.name);
-		    /* mark chils as dead */
-		    }
-	       
-	    }
-       }
-
+       
   printf("\n All of my children are dead... so bye....");
 
 
